bbcc61d254c72c12a9fb818b8e7cf29fee54900b
Joceliyn <jocevaldez98@gmail.com>
2024-02-22 19:35:38 -0800

    working on main.js

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
 const answer = Math.floor(Math.random() * 25) + 1;
-console.log(answer);
+const $gameForm = document.getElementById('gameForm');
+
+$gameForm = addEventListener('submit', handleGameForm);
+
+function handleGameForm(event) {
+    event.preventDefault();
+    const guess = event.target.elements.guess.value;
+    checkGuess(guess);
+}
+
+function checkGuess(guess) {
+    if (guess > answer) {
+        alert('Too high!, try again');
+    } else if (guess < answer) {
+        alert('Ahh, too low! Try again');
+    } else {
+        alert('Correct! You win!!!');
+    }
+}
